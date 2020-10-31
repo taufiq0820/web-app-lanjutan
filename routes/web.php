2cca120c9dfae5a62b525247b8ca4c066b0af9b3
unknown <taufiq0820>
2020-10-31 21:38:42 +0800

    file telah dirubah

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Controller1;
+use App\Http\Controllers\Controller2;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/satu", [Controller1::class, "one"]);
+Route::get("/dua", [Controller1::class, "two"]);
+Route::get("/tiga", [Controller1::class, "three"]);
+Route::get("/empat", [Controller2::class, "film"]);
+Route::get("/lima", [Controller2::class, "makanan"]);
+Route::get("/enam", [Controller2::class, "minuman"]);
+
+
+Route::get("/nama", function () {
+    return "<h1> nama saya Taufiqurrahman </h1>";
 });
+
+Route::get("/nim", function () {
+    return "<h1> NIM 1811102441094 </h1>";
+});
+
+Route::get("/kelas", function () {
+    return "<h1> kelas A </h1>";
+});
+
+Route::get("/nomor", function () {
+    return "<h1> nomor rumah 70 </h1>";
+});
+
+Route::get("/makanan", function () {
+    return "<h1> Geprek Mba Har </h1>";
+});
+?>
